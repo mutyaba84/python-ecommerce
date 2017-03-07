4fcdf58a1d69c4dc297b4ac5d353f00cf0baeb15
Carlo Fontanos <carl.fontanos@gmail.com>
2017-03-07 05:08:32 -0600

    Added comments.

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import logout as django_logout
+from django.contrib.auth import authenticate, login, logout as django_logout # Use alias like "django_logout" so that django doesnt get confused on which function to use.
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import generic
@@ -16,8 +16,24 @@ def contact(request):
     return render(request, 'ecommerce/contact.html')
 
 def user_login(request):
-    return render(request, 'ecommerce/user/login.html')
-	
+	if len(request.body) > 0: 
+		# Process the request if posted data are available
+		username = request.POST['username']
+		password = request.POST['password']
+		# Check username and password combination if correct
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			# Save session as cookie to login the user
+			login(request, user)
+			# Success, now let's login the user. 
+			return render(request, 'ecommerce/user/account.html')
+		else:
+			# Incorrect credentials, let's throw an error to the screen.
+			return render(request, 'ecommerce/user/login.html', {'error_message': 'Incorrect username and / or password.'})
+	else:
+		# No post data availabe, let's just show the page to the user.
+		return render(request, 'ecommerce/user/login.html')
+		
 def user_account(request):
     return render(request, 'ecommerce/user/account.html')
 	
@@ -28,5 +44,7 @@ def user_register(request):
     return render(request, 'ecommerce/user/register.html')
 	
 def logout(request):
+	# Clear the session cookies to logout the user. 
     django_logout(request)
-    return HttpResponseRedirect('/ecommerce/') # Redirect after logout
+	# Redirect after logout
+    return HttpResponseRedirect('/ecommerce/user/login')
