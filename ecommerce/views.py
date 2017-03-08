6880838dbe1a67b8b640151dfb35760f46be9626
Carlo Fontanos <carl.fontanos@gmail.com>
2017-03-08 16:39:32 +0800

    Removed unused import.

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -2,7 +2,6 @@ from django.contrib.auth import authenticate, login, logout as django_logout # U
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from django.views import generic
 from .forms import RegisterForm
 
 
