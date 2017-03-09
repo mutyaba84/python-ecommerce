e9ea33b3a3a79363d983d7dfb8974f9b53446efb
Carlo Fontanos <carl.fontanos@gmail.com>
2017-03-09 17:27:47 +0800

    Hooked new member table to the admin user model.

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from ecommerce.models import Member
+
+# Define an inline admin descriptor for Member model
+# which acts a bit like a singleton
+class MemberInline(admin.StackedInline):
+    model = Member
+    can_delete = False
+    verbose_name_plural = 'member'
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (MemberInline, )
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
