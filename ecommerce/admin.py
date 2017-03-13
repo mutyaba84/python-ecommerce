421142e2ce9a4d5186619971aefa24d9d3f544f5
Carlo Fontanos <carl.fontanos@gmail.com>
2017-03-13 15:10:18 +0800

    Integrated product and image field to the admin.

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
-from ecommerce.models import Member
+from .models import Member, Product, Image
 
 # Define an inline admin descriptor for Member model
 # which acts a bit like a singleton
@@ -11,10 +11,23 @@ class MemberInline(admin.StackedInline):
     can_delete = False
     verbose_name_plural = 'member'
 
-# Define a new User admin
 class UserAdmin(BaseUserAdmin):
     inlines = (MemberInline, )
+
+# Define inline image fields for the admin    
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 3
+
+class ProductAdmin(admin.ModelAdmin):  
+    # Add images to the add / edit product on the admin 
+    inlines = [ImageInline]
+    
+    # You can view more options here: https://docs.djangoproject.com/en/1.10/intro/tutorial07/
 
 # Re-register UserAdmin
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+
+# Register Product and ProductAdmin
+admin.site.register(Product, ProductAdmin)
