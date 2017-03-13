ce6e7ab2e3016fcb9fdebac4842f97b1b9718a13
Carlo Fontanos <carl.fontanos@gmail.com>
2017-03-13 15:09:42 +0800

    Added model for Products.

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -5,3 +5,18 @@ class Member(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=30)
     about = models.TextField()
+    
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    content = models.TextField()
+    excerpt = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    status = models.IntegerField(default=0)
+    date = models.DateTimeField('date published')
+    quantity = models.PositiveIntegerField()
+    author = models.PositiveIntegerField()
+    featured_image = models.CharField(max_length=300)
+
+class Image(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    path = models.CharField(max_length=300)
