d80794db64ec0242cc6a7793609426ab61614402
Carlo Fontanos <carl.fontanos@gmail.com>
2017-03-15 19:04:44 +0800

    Added media_root and media_url for referencing upload files.

--- myproject/settings.py
+++ myproject/settings.py
@@ -117,5 +117,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
 STATIC_URL = '/static/'
+
+# Location of uploaded image files on the server.
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
