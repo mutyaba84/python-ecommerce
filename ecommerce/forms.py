dab4a81072850b8e134db0bbdfc96e84cd06d88a
Carlo Fontanos <carl.fontanos@gmail.com>
2017-03-23 13:29:30 +0800

    Added id for intializing CKEditor.

--- ecommerce/forms.py
+++ ecommerce/forms.py
@@ -22,7 +22,7 @@ class AccountForm(forms.Form):
 	
 class CreateProductForm(forms.Form):
 	name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
-	content = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}))
+	content = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control', 'id': 'ck-editor-area'}))
 	excerpt = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control', 'rows': '7'}), required=False)
 	price = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Amount'}))
 	status = forms.CharField(widget=forms.Select(choices=(('1', 'Active'),('0', 'Inactive'),), attrs={'class':'form-control'}))
@@ -30,7 +30,7 @@ class CreateProductForm(forms.Form):
 	
 class UpdateProductForm(forms.Form):
 	name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
-	content = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control'}))
+	content = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control', 'id': 'ck-editor-area'}))
 	excerpt = forms.CharField(widget=forms.Textarea(attrs={'class':'form-control', 'rows': '7'}), required=False)
 	images = forms.CharField(widget=forms.FileInput(attrs={'class':'form-control image-input', 'accept': 'image/*', 'multiple': 'multiple'}), required=False)
 	price = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Amount'}))
